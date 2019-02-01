6b0ac85ec56da3a62003f5c57f35d8e620079299
Andrew Green <andrew.green-2@manchester.ac.uk>
2019-02-01 11:23:27 +0000

    Added setup.py to allow installation with pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+
+setup(name="pymining",
+      version="1.0",
+      description="Image based data mining in python",
+      author="Andrew Green",
+      author_email="andrew.green-2@manchester.ac.uk",
+      url="https://github.com/afg1/pymining",
+      packages=["pymining"],
+    )
